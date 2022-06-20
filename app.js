2f31cb3d5ea542554685a6e3485b1b77d83dd756
HeatherH <hherries@yahoo.com>
2022-06-20 10:00:45 -0600

    Validation and Alerts

--- app.js
+++ app.js
@@ -4,25 +4,46 @@ function Book(title, author, isbn) {
   this.author = author;
   this.isbn = isbn;
 }
+
 // UI Constructor
 function UI() {}
 
-// Add Book to List
+// Add Book To List
 UI.prototype.addBookToList = function(book) {
   const list = document.getElementById("book-list");
-
-  // Create <tr> Element
+  // Create tr element
   const row = document.createElement("tr");
 
-  // Insert Columns
+  // Insert cols
   row.innerHTML = `
-  <td>${book.title}</td>
-  <td>${book.author}</td>
-  <td>${book.isbn}</td>
-  <td><a href="#" class="delete">X</td>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a></td>
   `;
 
   list.appendChild(row);
+};
+
+// Show Alert
+UI.prototype.showAlert = function(message, className) {
+  // Create div
+  const div = document.createElement("div");
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector(".container");
+  // Get form
+  const form = document.querySelector("#book-form");
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout after 3 sec
+  setTimeout(function() {
+    document.querySelector(".alert").remove();
+  }, 3000);
 };
 
 // Clear Fields
@@ -31,24 +52,34 @@ UI.prototype.clearFields = function() {
   document.getElementById("author").value = "";
   document.getElementById("isbn").value = "";
 };
+
 // Event Listeners
 document.getElementById("book-form").addEventListener("submit", function(e) {
-  //Get Form Values
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const isbn = document.getElementById("isbn").value;
+  // Get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
 
-  // Instantiate Book
+  // Instantiate book
   const book = new Book(title, author, isbn);
 
   // Instantiate UI
   const ui = new UI();
 
-  // Add Book to List
-  ui.addBookToList(book);
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    // Error alert
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
 
-  // Clear Fields
-  ui.clearFields();
+    // Show success
+    ui.showAlert("Book Added!", "success");
+
+    // Clear fields
+    ui.clearFields();
+  }
 
   e.preventDefault();
 });
